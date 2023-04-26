a06038061e79625b4735f3fc3f9f873022c6f62c
GeekyCoder19 <110388756+GeekyCoder19@users.noreply.github.com>
2023-04-26 18:34:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,8 @@ function showAlert() {
 
 // toggle the visibility of the "Our Mission" section when the "Learn More" button is clicked
 function toggleMission() {
-  var element = document.getElementById("mission-section");
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
+  var element = document.querySelector("#mission-section");
+  element.classList.toggle("show");
 }
 
 // validate the contact form before submission
@@ -18,22 +14,22 @@ function validateForm() {
   var name = document.forms["contact-form"]["name"].value;
   var email = document.forms["contact-form"]["email"].value;
   var message = document.forms["contact-form"]["message"].value;
-  
-  if (name == "") {
+
+  if (!name) {
     alert("Please enter your name.");
     return false;
   }
-  
-  if (email == "") {
+
+  if (!email) {
     alert("Please enter your email address.");
     return false;
   }
-  
-  if (message == "") {
+
+  if (!message) {
     alert("Please enter a message.");
     return false;
   }
-  
+
   alert("Thank you for your message! We will get back to you soon.");
   return true;
 }
